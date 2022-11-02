ce402acb5a2b085a6541826ea4d409781371a577
Michael <michaeljsaenz@gmail.com>
2022-11-01 23:47:25 -0500

    add html template, update handlers to use db, setup db connectivity

--- go.mod
+++ go.mod
@@ -2,20 +2,24 @@ module github.com/michaelc0n/todo
 
 go 1.18
 
-require github.com/gofiber/fiber/v2 v2.39.0
+require (
+	github.com/cosmtrek/air v1.40.4
+	github.com/gofiber/fiber/v2 v2.39.0
+	github.com/gofiber/template v1.7.1
+	github.com/lib/pq v1.10.7
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/cosmtrek/air v1.40.4 // indirect
 	github.com/creack/pty v1.1.11 // indirect
-	github.com/fatih/color v1.10.0 // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.40.0 // indirect
--- server.go
+++ server.go
@@ -1,37 +1,83 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
+	_ "github.com/lib/pq"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
 )
 
 func main() {
-	app := fiber.New()
+	connStr := "postgresql://username:password@localhost/todos?sslmode=disable"
+	// Connect to database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	app.Get("/", indexHandler)
-	app.Post("/", postHandler)
-	app.Put("/update", putHandler)
-	app.Delete("/delete", deleteHandler)
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db)
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
+
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
+	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
+
+	app.Static("/", "./public")
 	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+
 }
 
-func indexHandler(c *fiber.Ctx) error {
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	var res string
+	var todos []string
+
+	rows, err := db.Query("SELECT * FROM todos")
+
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON("an error occured")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		rows.Scan(&res)
+		todos = append(todos, res)
+	}
+
+	return c.Render("index", fiber.Map{
+		"Todos": todos,
+	})
+}
+
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello")
 }
-func postHandler(c *fiber.Ctx) error {
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello")
 }
-func putHandler(c *fiber.Ctx) error {
-	return c.SendString("Hello")
-}
-func deleteHandler(c *fiber.Ctx) error {
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello")
 }
